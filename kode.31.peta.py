ea8422bf30d487928e18afbb21ad1cf0bc5df401
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-09-29 14:24:12 +0700

    Add files via upload

---
+++ kode.31.peta.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+skala = float(input("Tuliskan Nilai Skala 1 : "))
+jarakpeta = float(input("Tuliskan Jarak Pada Peta : "))
+
+# Mengkonversi
+peta = skala * jarakpeta
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Jarak sesungguhnya = %d Kilometer'  %(peta))
+print('==========================================================')
